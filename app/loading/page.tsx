23eb1e6c51db375dab0a1d256141c2af948dde86
d24755wt <wey.toh@student.manchester.ac.uk>
2023-11-06 14:38:51 +0000

    initialize the loading component

---
+++ app/loading/page.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return (
+    <div className="bg-white w-screen h-screen z-20">
+        <h1 className="text-red">This is the loading Page</h1>
+    </div>
+    )
+}
+
+export default Loading;
