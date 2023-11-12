8cdec6cff22f2779783b888193e788a272f3461e
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-11-12 23:50:09 +0000

    add the spline package

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -1,7 +1,7 @@
 
 const Loading = () => {
     return (
-    <div className="bg-white w-screen h-screen z-20">
+    <div className="bg-white absolute w-screen h-screen z-20">
         <h1 className="text-red">This is the loading Page</h1>
     </div>
     )
