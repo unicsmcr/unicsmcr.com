5f37640ae1bcacd6a5a70d9918f30e3e8fd98a4c
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-11-23 22:25:58 +0000

    navbar and footer components added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="bg-white bottom-0">
+        Footer
+    </div>
+    )
+}
+
+export default Footer
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,12 @@
-
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 function About() {
     return (
         <div>
+            <Navbar/>
             <h1 className='text-bold text-red-200 text-3xl'>THIS IS THE ABOUT PAGE...</h1>
+            <Footer/>
         </div>
     )
 }
