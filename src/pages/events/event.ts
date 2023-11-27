5f0dfd34307c99fbbd5312fb015783fd28c3e6dd
Paulina Dorobis <paulina.dorobis@student.manchester.ac.uk>
2023-11-27 15:23:12 +0000

    Change the types for time and date of the event

--- src/pages/events/event.ts
+++ src/pages/events/event.ts
@@ -1,14 +1,16 @@
+import moment from 'moment';
+
 export class UpcomingEvent {
     title: string;
     description: string;
     image: string;
-    date: Date;
-    time: Date;
+    date: moment.Moment;
+    time: moment.Moment;
     location: string;
     type: string[];
     link: string;
 
-    constructor(title: string, description: string, image: string, date: Date, time: Date, location: string, type: string[], link: string){
+    constructor(title: string, description: string, image: string, date: moment.Moment, time: moment.Moment, location: string, type: string[], link: string){
         this.title = title;
         this.description = description;
         this.image = image;
