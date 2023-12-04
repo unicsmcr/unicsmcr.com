a97eeeedb9f4807e556338d44a656cbaf0fd12af
d24755wt <wey.toh@student.manchester.ac.uk>
2023-12-04 15:16:18 +0000

    Implemented navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,61 @@
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+import React, { useState } from 'react';
+import { Menu, MenuProps, ConfigProvider } from 'antd';
+import "@fontsource/rubik-mono-one";
 
-export default Navbar
+const items: MenuProps['items'] = [
+  {
+    label: '/About',
+    key: 'about',
+  },
+  {
+    label: '/Events',
+    key: 'events',
+  },
+  {
+    label: '/Sponsors',
+    key: 'sponsors',
+  },
+  {
+    label: '/Committee',
+    key: 'committee',
+  },
+];
+
+const Navbar: React.FC = () => {
+  const [current, setCurrent] = useState('about');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <ConfigProvider 
+      theme={{
+        components: { 
+          Menu: {
+            itemColor: '#F1F1F1',
+            horizontalItemSelectedColor: "#9050B0",
+            itemHoverColor: '#9050B0',
+            fontFamily: 'Rubik Mono One',
+            
+          },
+        },
+      }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <img alt='UniCS logo' src='src/assets/logos/logo.png' width={60} height={60} className='mx-20'></img>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+          className="bg-unics-black"
+          
+        />
+      </div>
+    </ConfigProvider>
+  );
+};
+
+export default Navbar;
