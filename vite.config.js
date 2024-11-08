8ffcc2f43061901b375c1b637b7fd98659276568
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2024-11-08 23:02:33 +0000

    update base repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/unicsmcr/',
+  base: '/unicsmcr.com/',
   plugins: [react()],
 })
