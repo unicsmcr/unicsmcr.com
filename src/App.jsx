d28dd9e31e87d326f7b33bf646a9fef27746a180
Jacob <jacoblaity@me.com>
2024-11-28 17:51:19 +0000

    Animated FAQ section added
    Need to fill with proper questions and not placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Bee from "./Bee";
 import Committee from "./components/Committee";
+import Faq from "./components/Faq";
 
 function App() {
   return (
@@ -40,6 +41,8 @@ function App() {
         </p>
         <p>More information about May Ball coming soon!</p>
       </div>
+
+      <Faq />
 
       <Committee />
 
