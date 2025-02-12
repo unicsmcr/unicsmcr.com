1141c818aebb6edba3d37cd387db96f644e0171b
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2025-02-12 22:58:59 +0000

    organise and add legacy committee info (not images)

--- src/components/CommitteeMember.jsx
+++ src/components/CommitteeMember.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-const CommitteeMember = ({ year, name, role, email, image }) => {
-  const imagePath = `${year}/${image}`;
+const CommitteeMember = ({ year, name, role, social, email, image }) => {
+  const imagePath = `committee-photos/${year}/${image}`;
+
   return (
     <div className="role">
-      <img src={imagePath} className="committee" alt={`${name}, ${role}`} />
+      {social ? (
+        <a href={social} target="_blank" rel="noopener noreferrer">
+          <img src={imagePath} className="committee" alt={`${name}, ${role}`} />
+        </a>
+      ) : (
+        <img src={imagePath} className="committee" alt={`${name}, ${role}`} />
+      )}
       <p className="name">{name}</p>
       <p className="role-name">{role}</p>
-      <a href={`mailto:${email}`} className="email">
-        {email}
-      </a>
+      {email && (
+        <a href={`mailto:${email}`} className="email">
+          {email}
+        </a>
+      )}
     </div>
   );
 };
