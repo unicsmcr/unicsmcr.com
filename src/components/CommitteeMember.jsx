0a33fcc28bef86c240c34457285e944c0a964962
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2025-02-19 18:24:58 +0000

    add linkedin icon and link

--- src/components/CommitteeMember.jsx
+++ src/components/CommitteeMember.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 const CommitteeMember = ({ year, name, role, social, email, image }) => {
   const imagePath = `committee-photos/${year}/${image}`;
@@ -12,7 +13,21 @@ const CommitteeMember = ({ year, name, role, social, email, image }) => {
       ) : (
         <img src={imagePath} className="committee" alt={`${name}, ${role}`} />
       )}
-      <p className="name">{name}</p>
+      <div className="name-and-social">
+        <p className="name">{name}</p>
+        <div className="linkedin">
+          {social && (
+            <a
+              href={social}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkedin-icon"
+            >
+              <FaLinkedin />
+            </a>
+          )}
+        </div>
+      </div>
       <p className="role-name">{role}</p>
       {email && (
         <a href={`mailto:${email}`} className="email">
