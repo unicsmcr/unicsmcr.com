8991193b1982a66ac7c5a1d10bbfded91d848cec
Insiya Mullamitha <insiya.mullamitha@gmail.com>
2025-03-06 17:25:49 +0000

    make website preview component more responsive

--- src/components/WebsitePreview.jsx
+++ src/components/WebsitePreview.jsx
@@ -1,35 +1,16 @@
 import React from "react";
+import "./WebsitePreview.css";
 
 const WebsitePreview = ({ image, title, link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
       <div
-        style={{
-          width: "300px",
-          height: "240px",
-          borderRadius: "12px",
-          overflow: "hidden",
-          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.2)",
-          backgroundColor: "#1e1e1e",
-          color: "white",
-          fontFamily: "sans-serif",
-          cursor: "pointer",
-          transition: "transform 0.3s ease",
-        }}
+        class="preview-link-container"
         onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.03)")}
         onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
       >
         <img src={image} alt={title} style={{ width: "100%", height: "75%" }} />
-        <div
-          style={{
-            padding: "7px",
-            paddingBlock: "5px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            font: "12px",
-          }}
+        <div class="preview-link-title"
         >
           <span>{title}</span>
         </div>
